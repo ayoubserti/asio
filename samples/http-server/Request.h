aa60adf0bc83089117575c49db5cd51d08404613
Ayoub serti <ayb.serti@gmail.com>
2017-02-28 17:20:43 +0000

    request class

---
+++ samples/http-server/Request.h
@@ -0,0 +1,152 @@
+#pragma once 
+#include "http_parser.h"
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <functional>
+
+using namespace std;
+using namespace std::placeholders;
+
+
+
+
+
+class Request
+{
+	
+	http_method method_;
+	string url_;
+	typedef unordered_map<string, string> HeadersMap;
+	HeadersMap  headers_;
+	string method_str_;
+	string next_header_;
+	char* body_;
+	size_t body_len_;
+	
+
+public:
+	static int on_message_begin(http_parser *p)
+	{
+		return 0;
+	}
+
+	static int on_header_field(http_parser *p, const char *buf, size_t len)
+	{
+		reinterpret_cast<Request*>(p->data)->next_header_ = std::move(string(buf, len));  return 0;
+	}
+
+	static int on_header_value(http_parser *p, const char *buf, size_t len)
+	{
+		Request* o = reinterpret_cast<Request*>(p->data);
+		o->headers_[o->next_header_] = std::move(string(buf, len));  
+		return 0;
+	}
+
+
+	static int on_request_url(http_parser *p, const char *buf, size_t len){
+		Request* req = reinterpret_cast<Request*>(p->data);
+		http_parser_url url;
+		http_parser_url_init(&url);
+		cout << http_parser_parse_url(buf, len, 0, &url);
+		req->url_ = string(buf, len);
+		return 0;
+	}
+
+	static int on_body(http_parser *p, const char *buf, size_t len)
+	{
+		Request* req = reinterpret_cast<Request*>(p->data);
+		req->body_ = reinterpret_cast<char*>(::malloc(len));
+		req->body_len_ = len;
+		::memcpy(req->body_, buf, len);
+		return 0; 
+	}
+
+
+	static int on_headers_complete(http_parser *p) 
+	{
+
+		Request* o = reinterpret_cast<Request*>(p->data);
+		for (auto&i : o->headers_)
+		{
+			cout << i.first << "      " << i.second << endl;
+		}
+
+		return 0;
+	}
+	
+	static int on_message_complete(http_parser *p) 
+	{
+		Request* req = reinterpret_cast<Request*>(p->data);
+		req->method_str_ = http_method_str((http_method)p->method);
+		req->method_ = (http_method)p->method;
+
+		cerr << "Message Complete" << endl;
+		return 0;
+	}
+
+	static int on_chunk_header(http_parser *p){
+		return 0; 
+	}
+
+	static int on_chunk_complete(http_parser *p) {
+		return 0;
+	}
+
+	static int on_status(http_parser *p, const char *buf, size_t len)
+	{
+		return 0;
+
+	}
+
+	int parse(const char* buf,std::size_t len){
+		
+		http_parser parser;
+		http_parser_init(&parser, http_parser_type::HTTP_BOTH);
+		parser.data = this;
+		http_parser_settings settings;
+
+		settings.on_url = &Request::on_request_url;
+		settings.on_message_begin = &Request::on_message_begin;
+		settings.on_header_field = &Request::on_header_field;
+		settings.on_header_value = &Request::on_header_value;
+
+		settings.on_status = &Request::on_status;
+		settings.on_body = &Request::on_body;
+		settings.on_headers_complete = &Request::on_headers_complete;
+		settings.on_message_complete = &Request::on_message_complete;
+		settings.on_chunk_header = &Request::on_chunk_header;
+		settings.on_chunk_complete = &Request::on_chunk_complete;
+
+		int len_of_done = http_parser_execute(&parser, &settings, buf, len);
+		return len_of_done;
+		
+	}
+
+    size_t get_body_length()
+    {
+        return body_len_;
+    }
+
+    char* get_body(){
+        return body_;
+    }
+
+    HeadersMap get_headers()
+    {
+        return headers_;
+    }
+
+    string get_url()
+    {
+        return url_;
+    }
+    
+    string get_method_str()
+    {
+        return method_str_;
+    }
+	
+
+};
+    
--- samples/http-server/main.cc
+++ samples/http-server/main.cc
@@ -1,89 +1,11 @@
-#include "http_parser.h"
-#include <iostream>
-#include <string>
-#include <unordered_map>
-
-using namespace std;
+#include "Request.h"
 
 
-
-
-
-class Request
-{
-	
-	http_method method_;
-	string url_;
-	typedef unordered_map<string, string> HeadersMap;
-	HeadersMap  headers_;
-	string method_str_;
-	string next_header_;
-
-public:
-	int on_message_begin(http_parser *p)
-	{
-		cerr << "Message Begin" << endl;
-		Request* req = new Request;
-		p->data = req;
-		return 0;
-	}
-
-	int on_header_field(http_parser *p, const char *buf, size_t len)
-	{
-		reinterpret_cast<Request*>(p->data)->next_header_ = string(buf, len);  return 0;
-	}
-
-	int on_header_value(http_parser *p, const char *buf, size_t len)
-	{
-		Request* o = reinterpret_cast<Request*>(p->data);
-		o->headers_[o->next_header_] = string(buf, len);  
-		return 0;
-	}
-
-
-
-	
-};
-    
-    
-int request_url_cb(http_parser *p, const char *buf, size_t len){ 
-	Request* req = reinterpret_cast<Request*>(p->data);
-	http_parser_url url;
-	http_parser_url_init(&url);
-	cout << http_parser_parse_url(buf, len, 0, &url);
-	req->url_ = string(buf, len);
-	return 0; 
-}
-    
-int body_cb(http_parser *p, const char *buf, size_t len) { cout << string(buf, len) << endl;  return 0; }
-    
-int headers_complete_cb(http_parser *p) {
-		
-	Request* o = reinterpret_cast<Request*>(p->data);
-	for (auto&i : o->headers_)
-	{
-		cout << i.first << "      " << i.second << endl;
-	}
-
-	 return 0; }
-    
-int message_complete_cb(http_parser *p) { 
-	Request* req = reinterpret_cast<Request*>(p->data);
-	req->method_str_ = http_method_str((http_method)p->method);
-	req->method_ = (http_method)p->method;
-
-	cerr << "Message Complete" << endl;  return 0; }
-    
-int response_status_cb(http_parser *p, const char *buf, size_t len) { return 0; }
-    
-int chunk_header_cb(http_parser *p){ return 0; }
-    
-int chunk_complete_cb(http_parser *p) { return 0; }
 
 int main(void)
 {
 	
-	const char* data = "GET /hello.php?d=112&l=222 HTTP/1.1"
+	const char* data = "GET /hello.html HTTP/1.1"
 		"\r\nUser-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)"
 		"\r\nHost: www.tutorialspoint.com"
 		"\r\nAccept-Language: en-us"
@@ -93,29 +15,9 @@ int main(void)
 		"\r\nConnection: Keep-Alive\r\n\r\n dddd";
 	
 	size_t requestlen = strlen(data);
-	http_parser parser;
-	http_parser_settings settings;
 	
-	settings.on_url = request_url_cb;
-	settings.on_message_begin = message_begin_cb;
-	settings.on_header_field = header_field_cb;
-	settings.on_header_value = header_value_cb;
-	//settings.on_url = pause_request_url_cb;
-	settings.on_status = [](http_parser *p, const char *buf, size_t len) -> int{
-		cout << "Status " << buf << len;
-		return 0;
-	};
-	settings.on_body = body_cb;
-	settings.on_headers_complete = headers_complete_cb;
-	settings.on_message_complete = message_complete_cb;
-	settings.on_chunk_header = chunk_header_cb;
-	settings.on_chunk_complete = chunk_complete_cb;
-
-	//cout << http_parser_version();
-	http_parser_init(&parser, http_parser_type::HTTP_REQUEST);
-	
-
-	cout << "parsed data length:" <<  http_parser_execute(&parser, &settings, data, requestlen) << endl;
+	Request req;
+	req.parse(data, requestlen);
 	
 	cout << "Request Lenght: " << requestlen << endl;
 	return 0;
